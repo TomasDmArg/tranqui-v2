3c8511d7ab4653d39827361fc423649b623841bb
Tomas <tomasdimauro1@gmail.com>
2022-08-29 08:37:02 -0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -4,115 +4,8 @@ import Image from 'next/image';
 import { Dots } from '../components/Dots';
 import { HeaderResponsive } from '../components/Header';
 import React from 'react';
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    position: 'relative',
-    paddingTop: 120,
-    paddingBottom: 80,
-
-    '@media (max-width: 755px)': {
-      paddingTop: 80,
-      paddingBottom: 60,
-    },
-  },
-
-  inner: {
-    position: 'relative',
-    zIndex: 1,
-  },
-
-  dots: {
-    position: 'absolute',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-
-    '@media (max-width: 755px)': {
-      display: 'none',
-    },
-  },
-
-  dotsLeft: {
-    left: 0,
-    top: 0,
-  },
-
-  title: {
-    textAlign: 'center',
-    fontWeight: 800,
-    fontSize: 40,
-    letterSpacing: -1,
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    marginBottom: theme.spacing.xs,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-
-    '@media (max-width: 520px)': {
-      fontSize: 28,
-      textAlign: 'left',
-    },
-  },
-
-  highlight: {
-    color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6],
-  },
-
-  description: {
-    textAlign: 'center',
-
-    '@media (max-width: 520px)': {
-      textAlign: 'left',
-      fontSize: theme.fontSizes.md,
-    },
-  },
-
-  controls: {
-    marginTop: theme.spacing.lg,
-    display: 'flex',
-    justifyContent: 'center',
-
-    '@media (max-width: 520px)': {
-      flexDirection: 'column',
-    },
-  },
-
-  control: {
-    '&:not(:first-of-type)': {
-      marginLeft: theme.spacing.md,
-    },
-
-    '@media (max-width: 520px)': {
-      height: 42,
-      fontSize: theme.fontSizes.md,
-
-      '&:not(:first-of-type)': {
-        marginTop: theme.spacing.md,
-        marginLeft: 0,
-      },
-    },
-  },
-  control1: {
-    backgroundColor: '#45FFBD',
-    color: '#000',
-    '&:hover': {
-      backgroundColor: '#45FFBD',
-    },
-    '&:not(:first-of-type)': {
-      marginLeft: theme.spacing.md,
-    },
-
-    '@media (max-width: 520px)': {
-      height: 42,
-      fontSize: theme.fontSizes.md,
-
-      '&:not(:first-of-type)': {
-        marginTop: theme.spacing.md,
-        marginLeft: 0,
-      },
-    },
-  },
-}));
 
 export default function HeroText() {
-  const { classes } = useStyles();
-
   return (
     <React.Fragment>
       <Head>
@@ -125,14 +18,14 @@ export default function HeroText() {
           { link: '/professionals', label: 'Para profesionales' },
         ]
       } />
-      <Container className={classes.wrapper} size={1400}>
-        <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
-        <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
-        <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
-        <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+      <Container className="wrapper" size={1400}>
+        <Dots className="dots" style={{ left: 0, top: 0 }} />
+        <Dots className="dots" style={{ left: 60, top: 0 }} />
+        <Dots className="dots" style={{ left: 0, top: 140 }} />
+        <Dots className="dots" style={{ right: 0, top: 60 }} />
 
-        <div className={classes.inner}>
-          <Title className={classes.title}>
+        <div className="inner">
+          <Title className="title">
             <Image
               src="/logo.svg"
               alt="Mantine"
@@ -143,21 +36,21 @@ export default function HeroText() {
           </Title>
 
           <Container p={0} size={600}>
-            <Text size="lg" color="dimmed" className={classes.description}>
+            <Text size="lg" color="dimmed" className="description">
               ¿Estás pasando por una crisis? Tranqui, te ayudamos a superarla.
             </Text>
           </Container>
 
-          <div className={classes.controls}>
-            <Button className={classes.control1} size="lg">
+          <div className="controls">
+            <Button className="control1" size="lg">
               Necesito ayuda
             </Button>
-            <Button className={classes.control} size="lg" variant="default" color="gray">
+            <Button className="control" size="lg" variant="default" color="gray">
               Conozco a alguien
             </Button>
           </div>
-          <div className={classes.controls}>
-            <Button className={classes.control} size="lg" variant="subtle" color="gray">
+          <div className="controls">
+            <Button className="control" size="lg" variant="subtle" color="gray">
               Mas información
             </Button>
           </div>
